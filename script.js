e1d1535427e2858c34decf4ca5c8472818ff7e6f
Charles Fries <charliefries@icloud.com>
2026-03-23 01:27:21 -0700

    Add logo shake

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ window.intercomSettings = {
   api_base: "https://api-iam.intercom.io",
   app_id: "izrparmj",
 };
+
+const logo = document.getElementById("logo");
+logo.addEventListener("click", () => {
+  logo.classList.add("shake");
+  logo.addEventListener(
+    "animationend",
+    () => {
+      logo.classList.remove("shake");
+    },
+    { once: true },
+  );
+});
